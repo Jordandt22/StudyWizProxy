dd80c73f21e59cf6e3a70cc326dc80d5dd0357df
jordan truong <jordantruong@jordans-MacBook-Pro.local>
2022-11-11 21:20:28 -0800

    Copied from Old Windows Laptop to Macbook.

--- src/config/url.js
+++ src/config/url.js
@@ -4,6 +4,6 @@ module.exports = {
     // const randomNum = Math.floor(Math.random() * 1) + 1;
     const randomNum = 1;
     const APIServerURI = process.env[`API_SERVER_URI_${randomNum}`];
-    return NODE_ENV === "production" ? APIServerURI : "http://localhost:5000";
+    return NODE_ENV === "production" ? APIServerURI : "http://localhost:5050";
   },
 };
